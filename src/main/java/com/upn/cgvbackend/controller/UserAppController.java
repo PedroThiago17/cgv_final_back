ffcee6d6e364e4e33ba7a15447af210c93805cbc
Pedro Thiago Yossimar Vergara Vega <N00279437@upn.pe>
2022-05-31 23:29:27 -0500

    version estable

--- src/main/java/com/upn/cgvbackend/controller/UserAppController.java
+++ src/main/java/com/upn/cgvbackend/controller/UserAppController.java
@@ -42,17 +42,4 @@ public class UserAppController {
 			return ResponseEntity.badRequest()
 					.body("El usario con el DNI " + user.getDocumentUser() + " ya existe en el registro.");
 	}
-
-	@GetMapping("/bonita")
-	public String miBonitaIntro() {
-		return "<b>Hola mi bonita, bienvenida a este pequeño jueguito :3</b></br></br> Es muy fácil, solo tendrás que colocar un dato en esta URL.</br> Por ejemplo: ...api/v1/bonita/<b>*dato*</b> y darle enter, y así hasta que logres adivinar :3 </br> Suerte mi amor <3";
-	}
-
-	@GetMapping("/bonita/{fecha}")
-	public String miBonita(@PathVariable String fecha) {
-		if (fecha.equals("10-05-2022")) {
-			return "Muy bien mi amor <3 ! </br> Sabía que lo harías. Eres muy inteligente, bonita, hermosa, sexy :$ y sobre todo, muy inteligente. Eres puro arte <3</br> No olvides que te adoro mi hermosa, te adoro demasiado.</br> Gracias por participar xd";
-		} else
-			return "Te equivocaste :c </br> Pero no te preocupes mi amor! Te dejaré una pequeña pista...</br> <b>Es un dato muy importante para nosotros... usa guiones :3</b>";
-	}
 }
